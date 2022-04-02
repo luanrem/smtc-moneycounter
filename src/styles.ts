26fa85156ddd276c4596595cdcc20fe9c10ffd5f
luanrem <luanrem@gmail.com>
2022-04-01 23:35:46 -0300

    make the default configuration

---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  /* Created with https://www.css-gradient.com */
+  background: #4f36b7;
+  background: -webkit-linear-gradient(top left, #4f36b7, #ea25c5);
+  background: -moz-linear-gradient(top left, #4f36b7, #ea25c5);
+  background: linear-gradient(to bottom right, #4f36b7, #ea25c5);
+
+  width: 100%;
+  height: 100vh;
+`;
