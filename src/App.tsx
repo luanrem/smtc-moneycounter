abfc7612df8b800b6e2a172060fdde04f8c5725f
luanrem <luanrem@gmail.com>
2022-04-06 21:32:47 -0300

    Insert buttons and logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,17 @@ import NumberFormat from "react-number-format";
 
 import { AiOutlineArrowDown } from "react-icons/ai";
 import { MdLocalFireDepartment } from "react-icons/md";
+import { MdRefresh } from "react-icons/md";
+import { IoMdSettings } from "react-icons/io";
 import { theme } from "./theme";
-import { Button, Flex } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 function App() {
@@ -29,45 +38,83 @@ function App() {
 
   return (
     <ChakraProvider theme={theme}>
-      <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        flexDir="column"
+        w="100vw"
+        h="100vh"
+        align="center"
+        justify="center"
+      >
+        <Heading alignContent="flex-start" w="50rem">
+          <Image
+            margin="2rem 0 0 2rem"
+            h="3rem"
+            objectFit="cover"
+            src="./logo_rocketseat.png"
+            alt="rocketseatLogo"
+          />
+          <Text
+            margin="0 0 0 2rem"
+            color="blackAlpha.700"
+            fontFamily="Encode Sans Expanded"
+            fontSize="6xl"
+          >
+            Money Counter
+          </Text>
+        </Heading>
         <Flex
           w="100%"
           h="20rem"
           maxWidth="50rem"
           bg="whiteAlpha.900"
-          color="red.500"
-          fontSize="5rem"
-          align="center"
           borderRadius={8}
-          justify="space-evenly"
-          flexWrap="wrap"
+          flexDir="column"
+          justify="space-between"
         >
-          <AiOutlineArrowDown />{" "}
-          <NumberFormat
-            value={money}
-            prefix="R$"
-            type="text"
-            thousandsGroupStyle="thousand"
-            decimalSeparator="."
-            displayType="text"
-            thousandSeparator={true}
-            allowNegative={true}
-            decimalScale={0}
-            fixedDecimalScale={true}
-            isNumericString={false}
-            allowLeadingZeros={true}
-          />
-          <Button
-            leftIcon={<MdLocalFireDepartment />}
-            name="StartOrPause"
-            colorScheme="purple"
-            variant="outline"
-            width="6rem"
-            justifyContent="space-between"
-            onClick={handleStopStart}
+          <Flex color="gray.600" justifyContent="flex-end" h="2rem" w="auto">
+            <Button colorScheme="gray" variant="link">
+              <MdRefresh size="1.5rem" />
+            </Button>
+            <Button colorScheme="gray" variant="link">
+              <IoMdSettings size="1.5rem" />
+            </Button>
+          </Flex>
+          <Flex
+            color="red.500"
+            fontSize="5rem"
+            align="center"
+            borderRadius={8}
+            justify="space-evenly"
+            flexWrap="wrap"
           >
-            {isStarted ? "Stop" : "Start"}
-          </Button>
+            <AiOutlineArrowDown />
+            <NumberFormat
+              value={money}
+              prefix="R$"
+              type="text"
+              thousandsGroupStyle="thousand"
+              decimalSeparator="."
+              displayType="text"
+              thousandSeparator={true}
+              allowNegative={true}
+              decimalScale={0}
+              fixedDecimalScale={true}
+              isNumericString={false}
+              allowLeadingZeros={true}
+            />
+            <Button
+              leftIcon={<MdLocalFireDepartment />}
+              name="StartOrPause"
+              colorScheme="purple"
+              variant="outline"
+              width="6rem"
+              justifyContent="space-between"
+              onClick={handleStopStart}
+            >
+              {isStarted ? "Stop" : "Start"}
+            </Button>
+          </Flex>
+          <Flex h="2rem" />
         </Flex>
       </Flex>
     </ChakraProvider>
